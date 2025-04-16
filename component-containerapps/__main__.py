d6b65e8d79b76d4bb0aebabd11c6d1c268a6c678
MitchellGerdisch <mitch@pulumi.com>
2025-04-16 15:15:58 -0500

    fix namespace for component

--- component-containerapps/__main__.py
+++ component-containerapps/__main__.py
@@ -5,4 +5,4 @@ from appDeploy import AppDeploy # Push to container app
 # from appImageDeploy import AppImageDeploy # All of the above
 
 if __name__ == "__main__":
-    component_provider_host(name="gke", components=[AppImage, AppDeploy])
+    component_provider_host(name="containerapps", components=[AppImage, AppDeploy])
