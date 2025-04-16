cf692fefeaecf361c6c2cf1842e6626ec2543efb
MitchellGerdisch <mitch@pulumi.com>
2025-04-16 13:55:56 -0500

    typos

--- component-containerapps/__main__.py
+++ component-containerapps/__main__.py
@@ -1,8 +1,9 @@
 from pulumi.provider.experimental import component_provider_host
 
 from appImage import AppImage # Docker build and registry
-from appDeploy import AppDeploy # Push to container app
-from appImageDeploy import AppImageDeploy # All of the above
+# from appDeploy import AppDeploy # Push to container app
+# from appImageDeploy import AppImageDeploy # All of the above
 
 if __name__ == "__main__":
-    component_provider_host(name="gke", components=[AppImage, AppDeploy, AppImageDeploy])
+    component_provider_host(name="gke", components=[AppImage])
+    # component_provider_host(name="gke", components=[AppImage, AppDeploy, AppImageDeploy])
