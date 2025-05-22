b7e607d153113e9b5b74a43ee574d3c6cced4cc6
MitchellGerdisch <mitch@pulumi.com>
2025-05-22 13:06:48 -0500

    separate comp calls for dotnet

--- azure-comp-containerapps-cs/Program.cs
+++ azure-comp-containerapps-cs/Program.cs
@@ -4,5 +4,6 @@ using Pulumi;
 class Program
 {
     // static Task<int> Main() => Deployment.RunAsync<MyStackCompImage>();
-    static Task<int> Main() => Deployment.RunAsync<MyStackCompBuildDeploy>();
+    // static Task<int> Main() => Deployment.RunAsync<MyStackCompBuildDeploy>();
+    static Task<int> Main() => Deployment.RunAsync<MyStackCompImageAndDeploy>();
 }
